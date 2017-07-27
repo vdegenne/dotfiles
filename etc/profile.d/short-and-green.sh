18282b502f25a8e5e2bfacb1ea8e15bf37b2e85f
root <root@ns3266059.ip-5-39-79.eu>
2017-07-27 00:02:26 +0000

    add the git support

--- etc/profile.d/short-and-green.sh
+++ etc/profile.d/short-and-green.sh
@@ -1,15 +1,17 @@
-_dir_chomp () {                                                                           
-        local IFS=/ c=1 n d                                                               
-        local p=(${1/#$HOME/\~}) r=${p[*]}                                                
-        local s=${#r}                                                                     
-        while ((s > $2 && c < ${#p[*]} - 1))                                              
-        do                                                                                
-                d=${p[c]}                                                                 
-                n=1; [[ $d = .* ]] && n=2                                                 
-                ((s -= ${#d} - n))                                                        
-                p[c++]=${d:0:n}                                                           
-        done                                                                              
-        echo "${p[*]}"                                                                    
-}                                                                                         
-                                                                                          
-PS1='\[\033[01;32m\]$(_dir_chomp "$(pwd)" 3) \$\[\033[00m\] '
+source /usr/share/git-core/contrib/completion/git-prompt.sh
+
+_dir_chomp () {
+    local IFS=/ c=1 n d
+    local p=(${1/#$HOME/\~}) r=${p[*]}
+    local s=${#r}
+    while ((s > $2 && c < ${#p[*]} - 1))
+    do
+	d=${p[c]}
+	n=1; [[ $d = .* ]] && n=2
+	((s -= ${#d} - n))
+	p[c++]=${d:0:n}
+    done
+    echo "${p[*]}"
+}
+
+PS1='\[\033[01;32m\]\u[$(_dir_chomp "$(pwd)" 3)]\[\033[36m\]`__git_ps1`\[\033[00m\] \$ '
